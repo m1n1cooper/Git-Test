ca7b43eea420052b13aaf6357e0cf32d97c2a06d
James Topham <apple@apples-mbp.home>
2014-03-31 10:10:17 +0100

    Initial Commit

---
+++ Git Test/JTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JTAppDelegate.h
+//  Git Test
+//
+//  Created by James Topham on 31/03/2014.
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/JTViewController.h
@@ -0,0 +1,13 @@
+//
+//  JTViewController.h
+//  Git Test
+//
+//  Created by James Topham on 31/03/2014.
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTViewController : UIViewController
+
+@end

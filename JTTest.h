58ee37c122ffa0125893ae8eecea1b424cc2a732
James Topham <apple@apples-mbp.home>
2014-03-31 10:29:28 +0100

    added a test class

---
+++ JTTest.h
@@ -0,0 +1,13 @@
+//
+//  JTTest.h
+//  Git Test
+//
+//  Created by James Topham on 31/03/2014.
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTTest : NSObject
+
+@end
---
+++ JTTest.m
@@ -0,0 +1,13 @@
+//
+//  JTTest.m
+//  Git Test
+//
+//  Created by James Topham on 31/03/2014.
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import "JTTest.h"
+
+@implementation JTTest
+
+@end
